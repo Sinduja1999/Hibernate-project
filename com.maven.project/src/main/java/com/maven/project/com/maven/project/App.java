064d40f6eddca42ab4646c822f9278d2e57266e0
Sinduja1999 <120015577+Sinduja1999@users.noreply.github.com>
2023-03-09 22:05:12 +0530

    Add files via upload

---
+++ com.maven.project/src/main/java/com/maven/project/com/maven/project/App.java
@@ -0,0 +1,10 @@
+package com.maven.project.com.maven.project;
+
+
+public class App {
+	
+	public static void main( String[] args ){
+		BusManagement busobj = new BusManagement();
+		busobj.toReadInput();
+	}
+}
